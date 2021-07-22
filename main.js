5eaf2d82f429883b4278ddf6d15984699b55c7c2
Marina8989 <marina.mill209@gmail.com>
2021-07-22 11:26:03 -0700

    js form

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 const inputName = document.querySelector('#name');
+const email = document.getElementById('email');
+const emailAlarm = document.querySelector('.emailAlarm');
+const btn = document.getElementById('btn');
 
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    getName();
+
+    // if(email.value == '') {
+    //    emailAlarm.style.display = 'block';
+    //    email.style.border = '3px solid red';
+    //    setTimeout(() => {
+    //         emailAlarm.style.display = 'none';
+    //    }, 3000);
+    // }
+});
+
+function getName() {
+     if(inputName.value == ''){
+        inputName.style.border= '3px solid red';
+        inputName.style.background='white';
+        inputName.setAttribute('placeholder', 'Oops, this field is still empty')
+     }
+     inputName.addEventListener('click', () => {
+         inputName.style.border = '3px solid rgb(165, 165, 165)';
+     })  
+}
